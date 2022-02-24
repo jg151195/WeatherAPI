872224b4c4444dc7fd691b99259a4fa46a5f2594
jg151195 <62825246+jg151195@users.noreply.github.com>
2022-02-23 22:15:33 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,49 @@
+import City from '/City.js';
+
+const inputCity = document.querySelector('input').value;
+const container = document.getElementById('container');
+const date = new Date();
+
+const dayStatus = date.getHours() >= 6 && date.getHours() < 12 ? "MORNING" : date.getHours() >= 12 && date.getHours() < 18 ? "AFTERNOON" : "NIGHT";
+
+const city = new City(inputCity);
+
+
+
+    setInterval(await city.grabCity(inputCity), 5*60*1000);
+    setInterval(await city.grabWeather(inputCity), 5*60*1000);
+    console.log(city);
+
+
+    const headCity = document.createElement('h1');
+    headCity.textContent = city.name;
+    container.appendChild(headCity);
+
+    const img = document.createElement('img');
+    switch (dayStatus) {
+        case "MORNING":
+            img.src = "/morning.png";
+            container.style.backgroundImage = "url(/sunrise.jpg)";
+            break;
+        case "AFTERNOON":
+            img.src = "/afternoon.png";
+            container.style.backgroundImage = "url(/afternoon.jpg)";
+            break;
+        case "NIGHT":
+            img.src = "/night.png";
+            container.style.backgroundImage = "url(/night2.webp)";
+            break;
+    }
+    container.appendChild(img);
+    const spanTemp = document.createElement('h2');
+    spanTemp.textContent = city.temp;
+    container.appendChild(spanTemp);
+    const today = document.createElement('h3');
+    today.textContent = `Today, ${date.getHours()}:00`;
+    container.appendChild(today);
+    const spanHumidity = document.createElement('h3');
+    spanHumidity.textContent = `Humidity: ${city.humidity}`;
+    container.appendChild(spanHumidity);
+    const spanPressure = document.createElement('h3');
+    spanPressure.textContent = `Air Pressure: ${city.pressure}mb`;
+    container.appendChild(spanPressure);
---
+++ City.js
@@ -0,0 +1,43 @@
+export default class City {
+
+    constructor(name) {
+        this.name = name;
+        this.lat;
+        this.long;
+        this.temp;
+        this.humidity;
+        this.pressure;
+
+    }
+
+    async grabCity(name) {
+        try {
+            const apiCity = `http://api.openweathermap.org/geo/1.0/direct?q=${name}`;
+            const apiKey = '&appid=05b42a359cf920c47403016748ae9efb';
+            const fullAPICity = apiCity + apiKey;
+            const response = await fetch(fullAPICity);
+            const data = await response.json();
+            this.name = data[0].name;
+            this.lat = data[0].lat;
+            this.long = data[0].lon;
+            console.log(this.name, this.lat, this.long);
+        } catch (error) {
+            alert("Erro");
+        }
+
+    }
+
+    async grabWeather() {
+
+        const apiWeather = 'http://api.openweathermap.org/data/2.5/weather?';
+        const coords = `lat=${this.lat}&lon=${this.long}`;
+        const apiKey = '&appid=05b42a359cf920c47403016748ae9efb';;
+        const fullAPIWeather = apiWeather + coords + apiKey;
+        const response = await fetch(fullAPIWeather);
+        const data = await response.json();
+        this.temp = Math.round(data.main.temp - 273.15) + "°C";
+        this.humidity = `${data.main.humidity}%`;
+        this.pressure = `${data.main.pressure}`
+        console.log(data, this.temp, this.humidity);
+    }
+}
